9bcf53f4e8d694b9aa0ac0c57e5c52f8a6056ef9
Abraham R. Burnett <abeburnett@gmail.com>
2013-06-17 18:41:45 -0600

    Step 3 complete

--- web/dartwriter.dart
+++ web/dartwriter.dart
@@ -18,11 +18,48 @@ bool contentActive = true;
 String get sidebarClass => contentActive ? '' : 'active';
 String get mainClass => contentActive ? 'active' : '';
 
+/// An observed list of documents
+final List<Document> documents = toObservable([]);
+
+/// Create a document
+void createDocument(){
+  // Create a new document
+  var doc = new Document('Untitled', '');
+  documents.add(doc);
+  selectDocument(doc);
+}
+
+/// Delete a document
+void deleteDocument(Document doc){
+  documents.remove(doc);
 
 
+}
 
+/// Select a document
+void selectDocument(Document doc, {bool markActive: false}) {
+  if (documents.isEmpty){
+    activeDocument = null;
+  } else if (doc != null && documents.contains(doc) && doc != activeDocument){
+    // change active document
+    activeDocument = doc;
+  } else if (activeDocument != null && documents.contains(activeDocument)){
+    // stay on the active document and display the editing window
+    contentActive = markActive && doc == activeDocument;
+  } else {
+    // fall back to the last document
+    activeDocument = documents.last;
+  }
+}
 
 /// Starts the application
 void main(){
+
+  /// Create a new document if there are none
+  if (documents.isEmpty) createDocument();
+
+  /// Select the first document in the list of documents
+  selectDocument(documents.first);
+
 
 }
